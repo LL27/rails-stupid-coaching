6d180f1b5081dc8872b071fc6bc2752fa84ee75a
Lital Levin <lital.levin@gmail.com>
2020-02-10 12:12:25 +0100

    stupid coach -style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,9 @@ class QuestionsController < ApplicationController
 
   def answer
     @answer = "I don't care, get dressed and go to work!"
+
     @question = params["question"]
+
     if @question == "I am going to work"
       @answer = "Great!"
     elsif @question.include?("?")
